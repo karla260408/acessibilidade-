bd4ebf3f1a9e81ccbc789992f67dace9b38acc6e
karla260408 <166656917+karla260408@users.noreply.github.com>
2025-03-24 11:23:13 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao document.getElementById('diminuir-fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
 
